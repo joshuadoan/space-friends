0b3bce1ae3e845f38e4b63714356c5e78bb96522
Joshua Doan <insaddingaround@gmail.com>
2024-02-18 13:10:39 -0800

    fix bug

--- src/classes/Laborer.ts
+++ src/classes/Laborer.ts
@@ -58,12 +58,7 @@ export class Laborer extends Base {
         break;
       case LaborerState.Home:
         this.transact(-1);
-        if (this.status.health < 100) {
-          this.setStatus({
-            health: this.status.health + 1,
-          });
-        }
-        if (this.status.stuff < 1 && this.status.health > 99) {
+        if (this.status.stuff < 1) {
           this.dispatch(LaborerAction.GoToWork);
         }
         break;

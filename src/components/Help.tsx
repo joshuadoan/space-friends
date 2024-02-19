1de8e84baf37e2548db0cb685d94d5e5731f6ff3
Joshua Doan <insaddingaround@gmail.com>
2024-02-19 12:19:40 -0800

    fix

--- src/components/Help.tsx
+++ src/components/Help.tsx
@@ -6,7 +6,6 @@ import help from "../../readme.md";
 const Help = () => {
   return (
     <div className="prose lg:prose-base prose-pink  rounded-md p-4 overflow-auto flex flex-col h-full bg-white bg-opacity-90">
-      <h1>Readme</h1>
       <Markdown remarkPlugins={[remarkGfm]}>{help}</Markdown>
     </div>
   );

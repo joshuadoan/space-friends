19ee8d976ec765c5d51b209e4f32011bacd993e9
Joshua <insaddingaround@gmail.com>
2024-02-23 18:06:50 -0800

    More padding for help page to align with nav

--- src/components/Help.tsx
+++ src/components/Help.tsx
@@ -5,7 +5,7 @@ import help from "../../readme.md";
 
 const Help = () => {
   return (
-    <div className="prose text-white prose-headings:text-white overflow-auto prose-img:border prose-img:border-white prose-a:text-white p-4">
+    <div className="prose text-white prose-headings:text-white overflow-auto prose-img:border prose-img:border-white prose-a:text-white p-6">
       <Markdown remarkPlugins={[remarkGfm]}>{help}</Markdown>
     </div>
   );
